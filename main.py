c532d2e3b8062867ca8507ffd28543da125d3aae
ernesto <evoliveira@inf.ufrgs.br>
2021-11-08 19:16:50 -0300

    Create problem instance class

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello World!")
+class airplaneTrips:
+    def __init__(self, nPeople, kPlanes, cIndividual, cPair, pWeights):
+        self.nPeople = nPeople
+        self.kPlanes = kPlanes
+        self.cIndividual = cIndividual
+        self.cPair = cPair
+        self.pWeights = pWeights
+        self.PCapacity = calculatePlaneCapacity(pWeights, kPlanes)
+
+    def calculatePlaneCapacity(pWeights, kPlanes):
+        planeCapacity = [None] * kPlanes
+        totalWeight = sum(pWeights)
+        for k in range(kPlanes):
+            planeCapacity[k] = 0.8 * (totalWeight/k)
+        return planeCapacity
+
+
+
