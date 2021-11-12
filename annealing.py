671c64163ab19e9d3e5a3cc35bfa437f52482677
ernesto <evoliveira@inf.ufrgs.br>
2021-11-12 12:04:43 -0300

    Add comment for TODO

--- annealing.py
+++ annealing.py
@@ -1,10 +1,10 @@
 import planetrips as va
 
 def greedySolution(instance : va.Instance):
-    print(instance)
+    #TODO: implement greedy algorithm
+    print(instance) 
 
 def main():
-
     greedySolution(va.readInstance(1))
 
 if __name__ == "__main__":
