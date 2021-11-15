717e1c0a3db0990fb3f2d976fc5a2f4b8f115de0
ricco <ricco23052000@gmail.com>
2021-11-15 16:37:51 -0300

    Modularize different algorithms

--- annealing.py
+++ annealing.py
@@ -1,36 +1,12 @@
-import planetrips as va
 from contextlib import redirect_stdout #only for outputting debug to log file, may be deleted later
 import numpy as np
+import planetrips as va
+import greedysol as greedy
 
-def greedySolution(inst : va.Instance): #finds an initial greedy solution that will be optimized with simulated annealing
-    availableSpace = inst.PCapacity.tolist()
-    availablePeople = list(range(inst.nPeople))
-    sol = va.Solution(inst.kPlanes, inst.nPeople)
-    sortedPlanes = list(range(inst.kPlanes))
-    sortedPlanes.sort(key = lambda x:availableSpace[x])
-    for plane in sortedPlanes:
-        person = max(availablePeople,
-                           key=lambda x:inst.cIndividual[x]/inst.pWeights[x], default = -1)
-        if person != -1 and inst.pWeights[person] <= availableSpace[plane]:
-            sol.allocate(person, plane)
-            availableSpace[plane] -= inst.pWeights[person]
-            availablePeople.remove(person)
-            planeHasSpace = True
-            while(planeHasSpace):
-                friend = max(availablePeople,
-                                   key=lambda x:((inst.cIndividual[x] + inst.cPair[person][x])\
-                                   /inst.pWeights[x]) * (x!=person), default = -1)
-                if friend != -1 and inst.pWeights[friend] <= availableSpace[plane]:
-                    sol.allocate(friend, plane)
-                    availableSpace[plane] -= inst.pWeights[friend]
-                    availablePeople.remove(friend)
-                else:
-                    planeHasSpace = False
-    return sol
 
 def main():
     instance = va.readInstance(1)
-    solution = greedySolution(instance)
+    solution = greedy.greedySolution(instance)
     print(instance.isFeasible(solution))
 
 if __name__ == "__main__":
---
+++ greedysol.py
@@ -0,0 +1,27 @@
+import planetrips as va
+
+def greedySolution(inst : va.Instance): #finds an initial greedy solution that will be optimized with simulated annealing
+    availableSpace = inst.PCapacity.tolist()
+    availablePeople = list(range(inst.nPeople))
+    sol = va.Solution(inst.kPlanes, inst.nPeople)
+    sortedPlanes = list(range(inst.kPlanes))
+    sortedPlanes.sort(key = lambda x:availableSpace[x])
+    for plane in sortedPlanes:
+        person = max(availablePeople,
+                           key=lambda x:inst.cIndividual[x]/inst.pWeights[x], default = -1)
+        if person != -1 and inst.pWeights[person] <= availableSpace[plane]:
+            sol.allocate(person, plane)
+            availableSpace[plane] -= inst.pWeights[person]
+            availablePeople.remove(person)
+            planeHasSpace = True
+            while(planeHasSpace):
+                friend = max(availablePeople,
+                                   key=lambda x:((inst.cIndividual[x] + inst.cPair[person][x])\
+                                   /inst.pWeights[x]) * (x!=person), default = -1)
+                if friend != -1 and inst.pWeights[friend] <= availableSpace[plane]:
+                    sol.allocate(friend, plane)
+                    availableSpace[plane] -= inst.pWeights[friend]
+                    availablePeople.remove(friend)
+                else:
+                    planeHasSpace = False
+    return sol
