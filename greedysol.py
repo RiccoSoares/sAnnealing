3b11e0c49bd6f690eea396fd504ce641db908063
ernesto <evoliveira@inf.ufrgs.br>
2021-11-16 00:59:09 -0300

    Add comments for greedy solution

--- greedysol.py
+++ greedysol.py
@@ -3,23 +3,29 @@ from solution import Solution
 
 def greedySolution(inst : va.Instance): #finds an initial greedy solution that will be optimized with simulated annealing
     availablePeople = list(range(inst.nPeople))
-    sol = Solution(inst)
+    solution = Solution(inst)
     for plane in range(inst.kPlanes):
+
         person = max(availablePeople,
                            key=lambda x:inst.cIndividual[x]/inst.pWeights[x], default = -1)
-        if person != -1 and inst.pWeights[person] <= sol.freeSpace[plane]:
-            sol.allocate(person, plane)
-            sol.freeSpace[plane] -= inst.pWeights[person]
+        #returns person with biggest value/weight ratio, or -1 if there is no person available
+
+        if person != -1 and inst.pWeights[person] <= solution.freeSpace[plane]:
+            #if person fits in plane, allocate them, else go to next plane
+            solution.allocate(person, plane)
+            solution.freeSpace[plane] -= inst.pWeights[person]
             availablePeople.remove(person)
             planeHasSpace = True
             while(planeHasSpace):
                 friend = max(availablePeople,
                                    key=lambda x:((inst.cIndividual[x] + inst.cPair[person][x])\
                                    /inst.pWeights[x]) * (x!=person), default = -1)
-                if friend != -1 and inst.pWeights[friend] <= sol.freeSpace[plane]:
-                    sol.allocate(friend, plane)
-                    sol.freeSpace[plane] -= inst.pWeights[friend]
+                #return friend with biggest value/weight ratio, or -1 if there is no person available
+                if friend != -1 and inst.pWeights[friend] <= solution.freeSpace[plane]:
+                    #if friend fits, allocate them, else go to next plane
+                    solution.allocate(friend, plane)
+                    solution.freeSpace[plane] -= inst.pWeights[friend]
                     availablePeople.remove(friend)
                 else:
                     planeHasSpace = False
-    return sol
+    return solution
