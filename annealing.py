6ecc5054bb9f5419b3a78cb4048e3f712c6da5f1
ernesto <evoliveira@inf.ufrgs.br>
2021-11-16 12:20:32 -0300

    Create basis for metropolis

--- annealing.py
+++ annealing.py
@@ -1,67 +1,61 @@
-import numpy as np
 import math
 import random
+from copy import deepcopy
 import planetrips as va
-import greedysol as greedy
-from sty import fg,bg
-MIN_TEMPERATURE = 0.01
+from solution import Solution
+from greedysol import greedySolution
+import numpy as np
+MIN_TEMPERATURE = 10
 COOLING_RATE = 0.8
 
 def flipCoin(prob: float): #returns the result of a coin flip (true or false) with probability equals prob
-    
     return random.random() > prob
 
 def calcInitialTemp(inst: va.Instance): #calculates the initial temp for the algorithm, following the given specifications.
     #not implemented yet
-    
-    return 1
+    return 100000
 
 def calcIParameter(inst: va.Instance): #calculates the I parameter, following the given specifications.
-    
-    return inst.nPeople * inst.kPlanes
+    return inst.nPeople * inst.kPlanes *100
+
+def metropolis(sol : Solution, temp : float, iterations : int):
+    neighbourhood = sol.listNeighbours()
+    for i in range(iterations):
+        delta = sol.value - neighbourhood[i].value
+        if delta > 0:
+            sol = neighbourhood[i]
+            neighbourhood = sol.listNeighbours()
+        neighbourhood
 
 def simulatedAnnealing(inst: va.Instance): #inst arg represents an initial solution given by greedy algorithm.
-    temp = calcInitialTemp
-    current_sol = inst
+    temp = calcInitialTemp(inst)
+    current = greedySolution(inst)
+    candidate = deepcopy(current)
+
     I = calcIParameter(inst) #corresponds to the number of iterations without changing the temp value.
-    
     while (temp > MIN_TEMPERATURE):
-        for i in range(I):
-            current_eval = va.valuateSolution(current_sol)
-            candidate = va.returnNeighbour(current_sol)
-            candidate_eval = va.evaluateSolution(candidate)
-            delta = candidate_eval - current_eval
-            
+        for _ in range(I):
+            current = deepcopy(candidate)
+            candidate.randomNeighbourStep()
+            delta = candidate.value - current.value
             if delta > 0:
-                current_sol = candidate
-            else:
-                accept_prob = pow(math.e, delta/temp)
+                current = deepcopy(candidate)
+            elif delta < 0:
+                accept_prob = pow(math.e,-delta/temp)
                 if flipCoin(accept_prob):
-                    current_sol = candidate
-        
+                    current = deepcopy(candidate)
+        print(current.value)
         temp = temp*COOLING_RATE #updates the temperature
-    
-    return current_sol
+    return current
     
 
 def main():
     instance = va.readInstance(1)
-    solution = greedy.greedySolution(instance)
-    np.set_printoptions(linewidth=2000)
-    print(solution.vMatrix)
-    while(True):
-        print()
-        print(bg.black + fg.green + (str(solution.vMatrix)))
-        solution.randomNeighbourStep()
-    """
-    print("Initial solution is feasable: ", solution.isFeasible())
-    print("Initial solution value: ", solution.value)
-    solution.randomNeighbourStep()
-    print("New Solution is feasable: ", solution.isFeasible())
-    print("New Solution value: ", solution.value)
-    #new_solution = simulatedAnnealing(solution)
-    #print(instance.isFeasible(new_solution))
-    """
+    greedy = greedySolution(instance)
+    np.set_printoptions(linewidth=1000)
+    #print(greedy.vMatrix)
+    for solution in greedy.listNeighbours():
+        print(solution.vMatrix)
 
 if __name__ == "__main__":
     main()
