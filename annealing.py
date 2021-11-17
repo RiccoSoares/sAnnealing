db3f3d93072fc32e2b363de2fb2b64d70bbb0104
ernesto <evoliveira@inf.ufrgs.br>
2021-11-17 13:23:41 -0300

    Implement debug mode for annealing

--- annealing.py
+++ annealing.py
@@ -5,22 +5,22 @@ import planetrips as va
 from greedysol import greedySolution
 from solution import Solution
 import numpy as np
-MIN_TEMPERATURE = 0.0001
-COOLING_RATE = 0.8
+MIN_TEMPERATURE = 100
+COOLING_RATE = 0.99
 
 def flipCoin(prob: float): #returns the result of a coin flip (true or false) with probability equals prob
     return random.random() < prob
 
 def calcInitialTemp(inst: va.Instance): #calculates the initial temp for the algorithm, following the given specifications.
     #not implemented yet
-    return 100000
+    return 50000
 
 def calcIParameter(inst: va.Instance): #calculates the I parameter, following the given specifications.
-    return inst.nPeople * inst.kPlanes 
+    return inst.nPeople#* inst.kPlanes 
 
-def metropolis(solution : Solution, temperature : float, iterations : int):
+def metropolis(solution : Solution, temperature : float, iterations : int, debug1, debug2):
     best = copy.copy(solution)
-    for _ in range(iterations):
+    for i in range(iterations):
         candidate = copy.copy(solution)
         candidate.randomNeighbourStep()
         delta = candidate.value - solution.value
@@ -30,6 +30,7 @@ def metropolis(solution : Solution, temperature : float, iterations : int):
                 best = copy.copy(solution)
         elif delta < 0:
             accept_prob = np.exp(delta/temperature)
+            print("  " + str("%.6f" % accept_prob) + "  " + str("%06d" % candidate.value) + "  " + str("%06d" % best.value) + "  " + str("%.2f" % temperature) + "  " + "{:04d}".format(i) + "/" + "{:04d}".format(iterations) + "  " + "{:04d}".format(debug1) + "/" + "{:04d}".format(debug2), end='\r')
             if flipCoin(accept_prob):
                 solution = copy.copy(candidate)
     return best
@@ -40,8 +41,8 @@ def simulatedAnnealing(inst: va.Instance): #inst arg represents an initial solut
 
     I = calcIParameter(inst) #corresponds to the number of iterations without changing the temp value.
     while (temp > MIN_TEMPERATURE):
-        for _ in range(I):
-            candidate = metropolis(current, temp, 10)
+        for i in range(I):
+            candidate = metropolis(current, temp, 100, i, I)
             delta = candidate.value - current.value
             if delta > 0:
                 current = copy.copy(candidate)
@@ -51,11 +52,9 @@ def simulatedAnnealing(inst: va.Instance): #inst arg represents an initial solut
 
 def main():
     instance = va.readInstance(1)
-    greedy = greedySolution(instance)
-    print("Initial solution: ", greedy.value)
-    new_solution = simulatedAnnealing(instance)
-    print("New value: ", new_solution.value)
-    print("Feasible?: ", new_solution.isFeasible())
+    solution = simulatedAnnealing(instance)
+    print("Value: ", solution.value)
+    print("Feasible?: ", solution.isFeasible())
     
 
 if __name__ == "__main__":
